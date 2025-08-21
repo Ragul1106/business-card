ab82c47c562c6d52a54428cfc9a9b4417df97014
Ragul116 <ragulranjith1106@gmail.com>
2025-08-21 12:51:01 +0530

    add image

--- digital_card/settings.py
+++ digital_card/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]  
+STATICFILES_DIRS = [BASE_DIR / "profile_card/static"]  
 STATIC_ROOT = BASE_DIR / "staticfiles"   
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
